2edc58639931629f101aed1d241ccad0e39d2990
Omar Benchennouf <obenchennouf@gmail.com>
2021-12-26 12:25:40 +0100

    add lombok dependency and do some refactoring

--- src/country/model/Country.java
+++ src/country/model/Country.java
@@ -1,5 +1,8 @@
 package country.model;
 
+import lombok.Data;
+
+@Data
 public class Country {
 	private Integer id;
 	private String name;
@@ -7,43 +10,4 @@ public class Country {
 	private String devise;
 	private String greetings;
 	
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getCode() {
-		return code;
-	}
-	
-	public void setCode(String code) {
-		this.code = code;
-	}
-	
-	public String getDevise() {
-		return devise;
-	}
-	
-	public void setDevise(String devise) {
-		this.devise = devise;
-	}
-	
-	public String getGreetings() {
-		return greetings;
-	}
-	
-	public void setGreetings(String greetings) {
-		this.greetings = greetings;
-	}
 }
