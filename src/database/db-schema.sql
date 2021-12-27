8c0ceff7703f9e0b499e1be44c2e0442314678e2
Omar Benchennouf <obenchennouf@gmail.com>
2021-12-27 12:24:10 +0100

    add continent table and initialize it with data

--- src/database/db-schema.sql
+++ src/database/db-schema.sql
@@ -1,11 +1,22 @@
 CREATE SCHEMA if not exists countries;
 DROP TABLE IF EXISTS country;
+DROP TABLE IF EXISTS continent;
+
+CREATE TABLE continent
+(
+	id     INT AUTO_INCREMENT PRIMARY KEY,
+	name   VARCHAR(250) NOT NULL,
+    code   VARCHAR(250) NOT NULL
+);
 
 CREATE TABLE country
 (
     id     INT AUTO_INCREMENT PRIMARY KEY,
+    continentId  INT,
     name   VARCHAR(250) NOT NULL,
     code   VARCHAR(250) NOT NULL,
     devise VARCHAR(250) DEFAULT NULL,
-    greetings VARCHAR(250) DEFAULT NULL
+    greetings VARCHAR(250) DEFAULT NULL,
+    FOREIGN KEY(continentId) REFERENCES continent(id)
 );
+
--- src/database/populate-database.sql
+++ src/database/populate-database.sql
@@ -1,4 +1,9 @@
-INSERT INTO country(name, code, devise, greetings) VALUES('France', 'fr', 'EURO','Bonjour');
-INSERT INTO country(name, code, devise, greetings) VALUES('Spain', 'es', 'EURO','Hola');
-INSERT INTO country(name, code, devise, greetings) VALUES('England', 'en', 'GBP','Hello');
-INSERT INTO country(name, code, devise, greetings) VALUES('Germany', 'de', 'EURO','Halo');
+INSERT INTO continent(name, code) VALUES('Europe', 'eu');
+INSERT INTO continent(name, code) VALUES('Africa', 'af');
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='eu'), 'France', 'fr', 'EURO','Bonjour');
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='eu'), 'Spain', 'es', 'EURO','Hola');
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='eu'), 'England', 'en', 'GBP','Hello');
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='eu'), 'Germany', 'de', 'EURO','Halo');
+
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='af'), 'Morocco', 'ma', 'DH','SALAM');
+INSERT INTO country(continentId, name, code, devise, greetings) VALUES((SELECT id FROM continent WHERE code='af'), 'EGYPT', 'eg', 'EP','SALAM');
