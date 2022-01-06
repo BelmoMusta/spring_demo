3299decab515a2bc9f53048502e551d481e0e838
bilal ahsana <bilalahsana@gmail.com>
2022-01-06 04:37:45 +0100

    menu interactif

--- src/country/app/App.java
+++ src/country/app/App.java
@@ -9,16 +9,56 @@ import java.util.Scanner;
 @SuppressWarnings("all")
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext =
-				new ClassPathXmlApplicationContext("beans/*.xml");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans/*.xml");
 		IServiceWorker serviceWorker = applicationContext.getBean(IServiceWorker.class);
-		while (true) {
-			System.out.print("Choisir une langue : ");
+		boolean exit = false;
+		while (!exit) {
+			System.out.println("Menu:************");
+			System.out.print("\n" +
+					"\t1- Pour l'ajout d'un nouveau pays tapper 1, exemple : FR,france,EURO,Bonjour!\n" +
+					"\t2- Pour lister les informations d'un pays, tapper 2.\n" +
+					"\t3- Pour supprimer un pays, tapper 3.\n" +
+					"\t4- Pour modifier des informations d'un pays, tapper 4.\n" +
+					"\t5- Pour lister tous les pays d'un continent, tapper 5.\n" +
+					"\t6- Pour sortir de l'application tapper 0.\n");
+			System.out.print("Entrer un numero:");
 			Scanner inputFromConsole = new Scanner(System.in);
-			String language = inputFromConsole.next();
-			serviceWorker.dealWithCountryByCode(language);
-			
+			String number = inputFromConsole.next();
+			String code = null;
+			switch (number) {
+				case "1":
+					System.out.print("entrer les informations du pays:");
+					code = inputFromConsole.next();
+					serviceWorker.dealWithCountryByCode(code);
+					break;
+				case "2":
+					System.out.print("entrer le code du pays:");
+					code = inputFromConsole.next();
+					serviceWorker.dealWithCountryByCode(code);
+					break;
+				case "3":
+					System.out.print("entrer le code du pays:");
+					code = inputFromConsole.next();
+					serviceWorker.dealWithCountryByCode(code);
+					break;
+				case "4":
+					System.out.print("entrer le code du pays:");
+					code = inputFromConsole.next();
+					serviceWorker.dealWithCountryByCode(code);
+					break;
+				case "5":
+					System.out.print("entrer le code du continent:");
+					code = inputFromConsole.next();
+					serviceWorker.dealWithCountryByCode(code);
+					break;
+				case "0":
+					System.out.print("au revoir 👋👋👋 !!!");
+					exit = !exit;
+					break;
+				default:
+					System.out.println("choisir un numero entre 1 et 6!");
+					break;
+			}
 		}
 	}
-	
 }
