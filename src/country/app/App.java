73c182f37230c80abcf4c41369bd62a5bb087e39
chaymaemokhtari <mokhtari.chaymae.24@gmail.com>
2022-01-17 17:41:17 +0100

    exit_done

--- src/country/app/App.java
+++ src/country/app/App.java
@@ -13,18 +13,20 @@ public class App {
 				new ClassPathXmlApplicationContext("beans/*.xml");
 		IServiceWorker serviceWorker = applicationContext.getBean(IServiceWorker.class);
 		while (true) {
-			System.out.print("Enter a number between 0 and 5");
-			System.out.print("0 : Exit");
-			System.out.print("1 : Add a new country");
-			System.out.print("2 : List information about a country");
-			System.out.print("3 : Delete an existing country by code");
-			System.out.print("4 : Update information of an existing country by code");
-			System.out.print("5 : List all countries of a given continent");
-
+			System.out.print("\n");
+			System.out.println("Enter a number between 0 and 5");
+			System.out.println("0 : Exit");
+			System.out.println("1 : Add a new country");
+			System.out.println("2 : List information about a country");
+			System.out.println("3 : Delete an existing country by code");
+			System.out.println("4 : Update information of an existing country by code");
+			System.out.println("5 : List all countries of a given continent");
+			System.out.print("Code :");
 			Scanner inputFromConsole = new Scanner(System.in);
 			String language = inputFromConsole.next();
 			switch (language){
 				case "0":
+					System.exit(0);
 					break;
 				case "1":
 				break;
@@ -39,7 +41,6 @@ public class App {
 				default:
 					break;
 			}
-
 		}
 	}
 	
