ee111ad4d299265dae4efcca220ccfa9edf8e7d9
chaymaemokhtari <mokhtari.chaymae.24@gmail.com>
2022-01-17 17:33:08 +0100

    Menu created

--- src/country/app/App.java
+++ src/country/app/App.java
@@ -13,11 +13,33 @@ public class App {
 				new ClassPathXmlApplicationContext("beans/*.xml");
 		IServiceWorker serviceWorker = applicationContext.getBean(IServiceWorker.class);
 		while (true) {
-			System.out.print("Choisir une langue : ");
+			System.out.print("Enter a number between 0 and 5");
+			System.out.print("0 : Exit");
+			System.out.print("1 : Add a new country");
+			System.out.print("2 : List information about a country");
+			System.out.print("3 : Delete an existing country by code");
+			System.out.print("4 : Update information of an existing country by code");
+			System.out.print("5 : List all countries of a given continent");
+
 			Scanner inputFromConsole = new Scanner(System.in);
 			String language = inputFromConsole.next();
-			serviceWorker.dealWithCountryByCode(language);
-			
+			switch (language){
+				case "0":
+					break;
+				case "1":
+				break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				case "5":
+					break;
+				default:
+					break;
+			}
+
 		}
 	}
 	
