8ea993debc95b7d7a37453a24588fa9e110c10a4
Abdourahamane Besmor Bah <abdourahamanebesmorb@gmail.com>
2022-01-19 20:28:28 +0100

    Remplissage de la table continent

--- src/database/populate-database.sql
+++ src/database/populate-database.sql
@@ -1,4 +1,10 @@
-INSERT INTO country(name, code, devise, greetings) VALUES('France', 'fr', 'EURO','Bonjour');
-INSERT INTO country(name, code, devise, greetings) VALUES('Spain', 'es', 'EURO','Hola');
-INSERT INTO country(name, code, devise, greetings) VALUES('England', 'en', 'GBP','Hello');
-INSERT INTO country(name, code, devise, greetings) VALUES('Germany', 'de', 'EURO','Halo');
+INSERT INTO Continent(name, code) VALUES ('America', 'am')
+INSERT INTO Continent(name, code) VALUES ('Europe','eu')
+INSERT INTO Continent(name, code) VALUES ('Oceania', 'oc')
+INSERT INTO Continent(name, code) VALUES ('Africa', 'af')
+INSERT INTO Continent(name, code) VALUES ('Asia', 'as')
+
+INSERT INTO country(name, code, devise, greetings) VALUES('France', 'fr', 'EURO','Bonjour',2);
+INSERT INTO country(name, code, devise, greetings) VALUES('Spain', 'es', 'EURO','Hola',2);
+INSERT INTO country(name, code, devise, greetings) VALUES('England', 'en', 'GBP','Hello',2);
+INSERT INTO country(name, code, devise, greetings) VALUES('Germany', 'de', 'EURO','Halo',2);
