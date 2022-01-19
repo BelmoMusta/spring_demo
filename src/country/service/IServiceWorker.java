688f0f2f4aa2ec7c28aed1a162149800bcd748b5
NAIMI Mohammed <mohammednaimi1999@gmail.com>
2022-01-19 18:45:17 +0100

    Implémenter la fonctionnalité 02 pour lister les informations d'un pays par son code

--- src/country/service/IServiceWorker.java
+++ src/country/service/IServiceWorker.java
@@ -5,4 +5,6 @@ import country.model.Country;
 public interface IServiceWorker {
 	void dealWithCountryByCode(String language);
 	void InsertNewCountry(Country country,String nameOfContinet);
+	void ListCountryData(String code);
+	
 }
