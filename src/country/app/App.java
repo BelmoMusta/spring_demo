40cacf8fa1280bf43cc813a7533fe1324aa07191
amine abdel <amineabdel98@gmail.com>
2022-01-20 04:31:08 +0100

    intégration Hibernate

--- src/country/app/App.java
+++ src/country/app/App.java
@@ -1,24 +1,23 @@
 package country.app;
 
-import country.service.IServiceWorker;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import country.config.HBconfiguration;
 
 @SuppressWarnings("all")
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext =
-				new ClassPathXmlApplicationContext("beans/*.xml");
-		IServiceWorker serviceWorker = applicationContext.getBean(IServiceWorker.class);
+
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				HBconfiguration.class);
 		while (true) {
 			System.out.print("Choisir une langue : ");
 			Scanner inputFromConsole = new Scanner(System.in);
 			String language = inputFromConsole.next();
-			serviceWorker.dealWithCountryByCode(language);
-			
+
 		}
 	}
-	
+
 }
---
+++ src/country/config/CountryPackagesScane.java
@@ -0,0 +1,10 @@
+package country.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "country")
+public class CountryPackagesScane {
+
+}
